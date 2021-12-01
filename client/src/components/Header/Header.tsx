35416a3c9962dcddc1077cbc22ae6f3c52f88074
HANYUNSEONG <hanyunseong.dev@gmail.com>
2021-12-01 17:57:32 +0900

    update: 헤더 nav 추가 및 스타일 수정

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,9 +1,32 @@
+import Link from "next/link";
 import { HeaderWrapper } from "./styles";
+
+const menus = [
+  {
+    title: "로그인",
+    path: "/signin",
+  },
+  {
+    title: "회원가입",
+    path: "/signup",
+  },
+];
 
 function Header() {
   return (
     <HeaderWrapper>
-      <h1>Simple BBS</h1>
+      <h1>
+        <Link href="/">Simple BBS</Link>
+      </h1>
+      <nav>
+        <ul>
+          {menus.map((menu) => (
+            <li key={menu.path}>
+              <Link href={menu.path}>{menu.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </HeaderWrapper>
   );
 }
--- client/src/components/Header/styles.ts
+++ client/src/components/Header/styles.ts
@@ -1,14 +1,39 @@
 import styled from "@emotion/styled";
 
 export const HeaderWrapper = styled.header`
-  margin: 1rem;
-  padding: 1rem;
+  width: 100%;
+  padding: 1rem 2rem;
   background-color: #fff;
-  border-radius: 20px;
-  box-shadow: 0px 3px 10px 5px ${({ theme }) => theme.palette.GRAY_COLOR1};
+  border-radius: ${({ theme }) => theme.palette.DEFAULT_RADIUS};
+  box-shadow: 0px 3px 10px 5px ${({ theme }) => theme.palette.GRAY_COLOR2};
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   h1 {
     font-weight: 400;
-    text-align: center;
+  }
+
+  nav {
+    flex: 1;
+    text-align: right;
+    ul {
+      display: flex;
+      justify-content: flex-end;
+      li {
+        padding: 0 1rem;
+        &:not(:last-child) {
+          border-right: 1px solid #ddd;
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 480px) {
+    width: 90%;
+    margin: 0 auto;
+    flex-direction: column;
+    row-gap: 1rem;
   }
 `;
