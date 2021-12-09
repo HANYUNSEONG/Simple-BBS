15ab0596c1f7fe4a41ce0196aeb0a3e394408edb
HANYUNSEONG <hanyunseong.dev@gmail.com>
2021-12-09 12:12:30 +0900

    [server] update: cors 허용

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = 4000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
     .setTitle('Simple BBS')
