f74777bbdd887ee35a0fd04ec7f0ca421049b21b
HANYUNSEONG <hanyunseong.dev@gmail.com>
2022-01-25 10:31:20 +0900

    [client] update: viewer code higlight

--- client/src/components/Viewer/Viewer.tsx
+++ client/src/components/Viewer/Viewer.tsx
@@ -1,10 +1,19 @@
 import { Viewer as ToastViewer, ViewerProps } from "@toast-ui/react-editor";
+import Prism from "prismjs";
+import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight";
 import "@toast-ui/editor/dist/toastui-editor-viewer.css";
+import "prismjs/themes/prism.css";
+import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
 
 interface IViewerProps extends ViewerProps {}
 
 function Viewer(props: IViewerProps) {
-  return <ToastViewer {...props} />;
+  return (
+    <ToastViewer
+      {...props}
+      plugins={[[codeSyntaxHighlight, { highlighter: Prism }]]}
+    />
+  );
 }
 
 export default Viewer;
