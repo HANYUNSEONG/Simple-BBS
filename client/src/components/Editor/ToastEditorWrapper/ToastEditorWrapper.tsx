6ac11664a6513f9b6bc152061478f7c40a6ecb2a
HANYUNSEONG <hanyunseong.dev@gmail.com>
2022-01-27 13:46:04 +0900

    [client] refactor: Toast editor warpper

--- client/src/components/Editor/ToastEditorWrapper/ToastEditorWrapper.tsx
+++ client/src/components/Editor/ToastEditorWrapper/ToastEditorWrapper.tsx
@@ -8,6 +8,7 @@ import "@toast-ui/editor/dist/toastui-editor.css";
 import { ForwardedRef } from "react";
 import { useMutation } from "react-query";
 import { postFile } from "@/apis/file";
+import { HookCallback } from "@toast-ui/editor/types/editor";
 
 interface IToastEditorWrapper extends EditorProps {
   forwardedRef: ForwardedRef<any>;
@@ -15,25 +16,24 @@ interface IToastEditorWrapper extends EditorProps {
 function ToastEditorWrapper({ forwardedRef, ...props }: IToastEditorWrapper) {
   const postFileMutation = useMutation((file: File) => postFile(file));
 
+  const handleDropImage = (file: File | Blob, callback: HookCallback) => {
+    postFileMutation.mutateAsync(file as File, {
+      onSuccess(result) {
+        const response = result.data;
+        if (response) {
+          const { path, originalFileName } = response;
+          callback(process.env.NEXT_PUBLIC_API_URL + path, originalFileName);
+        }
+      },
+    });
+  };
+
   return (
     <Editor
       ref={forwardedRef}
       plugins={[[codeSyntaxHighlight, { highlighter: Prism }]]}
       hooks={{
-        addImageBlobHook: (file, callback) => {
-          postFileMutation.mutateAsync(file as File, {
-            onSuccess(result) {
-              const response = result.data;
-              if (response) {
-                const { path, originalFileName } = response;
-                callback(
-                  process.env.NEXT_PUBLIC_API_URL + path,
-                  originalFileName
-                );
-              }
-            },
-          });
-        },
+        addImageBlobHook: handleDropImage,
       }}
       {...props}
     />
