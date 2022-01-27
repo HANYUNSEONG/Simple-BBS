636242a28067fcd21d1b1c4d8b394d21cf0a530d
HANYUNSEONG <hanyunseong.dev@gmail.com>
2022-01-27 13:47:12 +0900

    [client] update: og meta tag 추가

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -6,6 +6,9 @@ function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Simple BBS" />
+        <meta property="og:site_name" content="Simple BBS" />
         <link rel="shortcut icon" href="/simple-bbs-logo.png" />
       </Head>
       <body>
